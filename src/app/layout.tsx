98c5e64a0d904ec0376524062ffe972ea9df0aa3
rolandowebdev <rolandopranata@gmail.com>
2023-11-13 13:50:49 +0700

    chore: remove unnecessary folder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import '@/globals.css'
+import './globals.css'
 
 import { PlusJakartaSans } from '@/lib/fonts'
 import type { Metadata } from 'next'
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,3 @@
-export default function Dashboard() {
+export default function Page() {
   return <h1>Dashboard Page</h1>
 }
